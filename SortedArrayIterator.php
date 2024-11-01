06ca109919f0a17e06cc10e1ae42184e4b471bfe
Patrik Farkas <farkas.patrik105@gmail.com>
2024-11-01 21:10:30 +0100

    Exercise 02

--- SortedArrayIterator.php
+++ SortedArrayIterator.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace IteratorExample;
 
-class SortedArrayIterator implements \Iterator {
+class SortedArrayIterator implements \Iterator
+{
     private array $array;
     private int $index = 0;
 
-    public function __construct(array $array) {
+    public function __construct(array $array)
+    {
         $this->array = $array;
         sort($this->array);
     }
--- utils.php
+++ utils.php
@@ -2,27 +2,30 @@
 
 declare(strict_types=1);
 
-function generateRandomNumbers(int $numberCount): array{
+function generateRandomNumbers(int $numberCount): array
+{
     $result = array();
-    for($i = 0; $i < $numberCount; $i++){
+    for ($i = 0; $i < $numberCount; $i++) {
         $result[] = rand(0, 999);
     }
 
     return $result;
 }
 
-function printWithForeach(iterable $iterable): void{
+function printWithForeach(iterable $iterable): void
+{
     print("Iterating over an " . gettype($iterable) . " with foreach:\n");
 
-    foreach ($iterable as $item){
+    foreach ($iterable as $item) {
         echo($item . "\n");
     }
 }
 
-function printIterator(\Iterator $iterator): void{
+function printIterator(\Iterator $iterator): void
+{
     print("Iterating over an iterator with object methods:\n");
 
-    while($iterator->valid()){
+    while ($iterator->valid()) {
         echo($iterator->current() . "\n");
         $iterator->next();
     }
